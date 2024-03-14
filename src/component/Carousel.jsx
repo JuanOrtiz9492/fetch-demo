9693e8104b224eb6f1db7f83b7f5f15c370cdbad
Nicolas Penagos Montoya <nicolas.penagos@globant.com>
2024-03-14 15:59:27 -0500

    bugs fixed

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -1,16 +1,39 @@
-import Card from "./Card"
-const Carousel = ({onLeftClick, onRightClick, elementList=[]})=> {
-    return (
-        <div className="flex w-full justify-center items-center">
-            <button onClick={onLeftClick} className="border-t-4 border-l-4 border-gray-300 w-8 h-8 -rotate-45"></button>
-                <div className="w-full h-auto flex flex-wrap">
-                    {elementList.map((element)=>(
-                        <Card key={element.name} img={element.img} name={element.name} onClick={element.onClick}/>
-                    ))}
-                </div>
-            <button onClick={onRightClick} className="border-t-4 border-r-4 border-gray-300 w-8 h-8 rotate-45"> </button>
-        </div>
-    )
-}
+import Card from "./Card";
+const Carousel = ({
+  onLeftClick,
+  onRightClick,
+  elementList = [],
+  nextPageExits,
+  prevPageExists,
+}) => {
+  return (
+    <div className="flex w-full justify-center items-center">
+      <button
+        onClick={onLeftClick}
+        className="border-t-4 border-l-4 border-gray-300 w-8 h-8 -rotate-45"
+        style={{ opacity: prevPageExists ? "1" : "0.15" }}
+      ></button>
 
-export default Carousel
+      <div className="w-full h-auto flex flex-wrap">
+        {elementList.map((element) => (
+          <Card
+            key={element.name}
+            img={element.img}
+            name={element.name}
+            onClick={element.onClick}
+          />
+        ))}
+      </div>
+
+      <button
+        onClick={onRightClick}
+        className="border-t-4 border-r-4 border-gray-300 w-8 h-8 rotate-45"
+        style={{ opacity: nextPageExits ? "1" : "0.15" }}
+      >
+        {" "}
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
