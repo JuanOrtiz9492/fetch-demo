12381be6a9cf26b88a0a16e9306ae7417ff7eb9c
Luis Eduardo Sanchez Reyes <luiseduardo.sanchez@globant.com>
2024-03-16 21:36:15 -0500

    demo

--- src/component/Sumary.jsx
+++ src/component/Sumary.jsx
@@ -1,30 +1,33 @@
-import { useEffect } from "react"
+import PropTypes from 'prop-types';
+const Summary = ({ data }) => {
+  const transformedStats = () => {
+    return data.stats.map((stat) => {
+      return {
+        value: stat.base_stat,
+        statName: stat.stat.name,
+      };
+    });
+  };
 
-const Summary = ({data})=> {
-    const transformedStats = ()=>{
-        return data.stats.map((data)=> {
-            return {
-                value: data.base_stat,
-                statName: data.stat.name
-            }
-        })
-    }
-    return (
-    <div className="flex w-96 bg-orange-400 px-10 py-5 rounded-md justify-center items-center" >
-        <div className="w-1/4 mr-5">
-            <img src={data.img}/>
-        </div>
-        <div className="w-3/4 flex flex-col">
-            {transformedStats().map((stat, idx)=>(
-                <div key={idx} className="flex text-white">
-                    <p>{stat.statName}:&nbsp;</p>
-                    <p>{stat.value}</p>
-                    
-                </div>
-            ))}
-        </div>
+  return (
+    <div className="flex w-96 bg-orange-400 px-10 py-5 rounded-md justify-center items-center">
+      <div className="w-1/4 mr-5">
+        <img src={data.img} alt="pokemon"></img>
+      </div>
+      <div className="w-3/4 flex flex-col">
+        {transformedStats().map((stat, idx) => (
+          <div key={idx} className="flex text-white">
+            <p>{stat.statName}:&nbsp;</p>
+            <p>{stat.value}</p>
+          </div>
+        ))}
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Summary
+Summary.propTypes = {
+    data: PropTypes.func.isRequired
+  };
+
+export default Summary;
