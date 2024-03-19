e42e591649c413b1049b8cf883b7e4676d0669a8
Daniela Becerra <140854855+daniela1234becerra@users.noreply.github.com>
2024-03-19 13:08:15 -0500

    Add files via upload

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -1,15 +1,14 @@
 import Card from "./Card"
-const Carousel = ({onLeftClick, onRightClick, elementList=[]})=> {
+const Carousel = ({ onLeftClick, onRightClick, elementList = [], isFirstPage, isLastPage }) => {
     return (
         <div className="flex w-full justify-center items-center">
-            <button onClick={onLeftClick} className="border-t-4 border-l-4 border-gray-300 w-8 h-8 -rotate-45"></button>
-                <div className="w-full h-auto flex flex-wrap">
-                    {elementList.map((element)=>(
-                        <Card key={element.name} img={element.img} name={element.name} onClick={element.onClick}/>
-                    ))}
-                </div>
-            <button onClick={onRightClick} className="border-t-4 border-r-4 border-gray-300 w-8 h-8 rotate-45"> </button>
-        </div>
+            {!isFirstPage && <button onClick={onLeftClick} className="border-t-4 border-l-4 border-gray-300 w-8 h-8 -rotate-45"></button>}
+            <div className="w-full h-auto flex flex-wrap">
+                {elementList.map((element) => (
+                    <Card key={element.name} img={element.img} name={element.name} onClick={element.onClick} />
+                ))}
+            </div>
+            {!isLastPage && <button onClick={onRightClick} className="border-t-4 border-r-4 border-gray-300 w-8 h-8 rotate-45"> </button>}        </div>
     )
 }
 
