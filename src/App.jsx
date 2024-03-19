7b59eb33e376f10e9c25deeea2cd4ec52e65fb13
Luis Fernando Mendez Marques <luisfernando.mendez@globant.com>
2024-03-19 09:34:24 -0500

    Carrousel issues fixed, pokemon images on cards added and validation of previousData button added

--- src/App.jsx
+++ src/App.jsx
@@ -5,53 +5,89 @@ import Summary from "./component/Sumary";
 import Carousel from "./component/Carousel";
 
 /**
- * 1. Fix Issues on Carrousel
- * 2. every card on Carrousel must have image
- * 3. the image must come from the api
+ * DONE 1. Fix Issues on Carrousel
+ * DONE 2. every card on Carrousel must have image
+ * DONE 3. the image must come from the api
  * you can use UseEffect or UseState to solve the issues on the project
  */
 
-// test
+/*+
+  REFERENCES
+  https://pokeapi.co/docs/v2#pokemon - API Documentation
+  https://www.freecodecamp.org/espanol/news/como-usar-async-await-para-escribir-un-codigo-mejor-en-javascript/
+
+*/
+
+const POKEMON_PER_PAGE = 6;
 
 function App() {
   const [pokeList, setPokeList] = useState([]);
   const [selectedPoke, setSelectedPoke] = useState({});
   const [page, setPage] = useState(0);
   const [isDarkMode, setIsDarkMode] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
-      const data = await axiosInstance.get("/pokemon?limit=6&offset=0");
-      setPokeList(data.data.results);
+      const data = await axiosInstance.get("/pokemon?limit=6&offset=`${page}`");
+      pokeBetterData(data.data.results);
     };
     fetchData();
   }, []);
 
-  const getInfo = async (url) => {
-    const data = await axiosInstance.get(url);
-    setSelectedPoke({
-      img: data.data.sprites.front_default,
-      stats: data.data.stats,
-    });
+  useEffect(() => {
+    const fetchPageData = async () => {
+      const data = await axiosInstance.get(
+        // 6 is the limit of pokemon per page. So, we need to multiply the page number by 6 to get the next page pokemons
+        `/pokemon?limit=6&offset=${page * POKEMON_PER_PAGE}`
+      );
+      pokeBetterData(data.data.results);
+    };
+    fetchPageData();
+  }, [page]);
+
+  const pokeBetterData = async (data) => {
+    const pokemonData = [];
+    for (const pokemon of data) {
+      try {
+        const response = await axiosInstance.get(pokemon.url);
+        pokemonData.push({
+          name: pokemon.name,
+          img: response.data.sprites.front_default,
+          url: pokemon.url,
+          stats: response.data.stats,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    setPokeList(pokemonData);
   };
+
   const getPokemonList = () => {
     return pokeList.map((pokemon) => {
       return {
         name: pokemon.name,
-        img: "/whos.jpg",
-        onClick: () => getInfo(pokemon.url),
+        img: pokemon.img,
+        onClick: () => getInfo(pokemon),
       };
     });
   };
-  const onNextPage = async () => {
-    setPage(page + 1);
-    const data = await axiosInstance.get(`/pokemon?limit=6&offset=${page}`);
-    setPokeList(data.data.results);
+
+  const getInfo = (poke) => {
+    setSelectedPoke({
+      img: poke.img,
+      stats: poke.stats,
+    });
   };
-  const onPrevPage = async () => {
-    setPage(page - 1);
-    const data = await axiosInstance.get(`/pokemon?limit=6&offset=${page}`);
-    setPokeList(data.data.results);
+
+  const onNextPage = () => {
+    setPage((prev) => prev + 1);
   };
+
+  const onPrevPage = () => {
+    setPage((prev) => prev - 1);
+  };
+
   return (
     <div
       className={`flex flex-col w-full h-screen items-center ${
@@ -76,7 +112,8 @@ function App() {
         PokeDex
       </h1>
       <Carousel
-        onLeftClick={onPrevPage}
+        // Only allow to go back if the page is greater than 0 (have pokemons to show)
+        onLeftClick={page > 0 ? onPrevPage : null}
         onRightClick={onNextPage}
         elementList={getPokemonList()}
       />
--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -1,16 +1,33 @@
-import Card from "./Card"
-const Carousel = ({onLeftClick, onRightClick, elementList=[]})=> {
-    return (
-        <div className="flex w-full justify-center items-center">
-            <button onClick={onLeftClick} className="border-t-4 border-l-4 border-gray-300 w-8 h-8 -rotate-45"></button>
-                <div className="w-full h-auto flex flex-wrap">
-                    {elementList.map((element)=>(
-                        <Card key={element.name} img={element.img} name={element.name} onClick={element.onClick}/>
-                    ))}
-                </div>
-            <button onClick={onRightClick} className="border-t-4 border-r-4 border-gray-300 w-8 h-8 rotate-45"> </button>
-        </div>
-    )
-}
+import Card from "./Card";
+const Carousel = ({ onLeftClick, onRightClick, elementList = [] }) => {
+  return (
+    <div className="flex w-full justify-center items-center">
+      <button
+        // Only show the button if the onLeftClick is not null
+        style={{
+          visibility: onLeftClick ? "visible" : "hidden",
+        }}
+        onClick={onLeftClick}
+        className="border-t-4 border-l-4 border-gray-300 w-8 h-8 -rotate-45"
+      ></button>
+      <div className="w-full h-auto flex flex-wrap">
+        {elementList.map((element) => (
+          <Card
+            key={element.name}
+            img={element.img}
+            name={element.name}
+            onClick={element.onClick}
+          />
+        ))}
+      </div>
+      <button
+        onClick={onRightClick}
+        className="border-t-4 border-r-4 border-gray-300 w-8 h-8 rotate-45"
+      >
+        {" "}
+      </button>
+    </div>
+  );
+};
 
-export default Carousel
+export default Carousel;
